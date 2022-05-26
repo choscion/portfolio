19a6e2591e677d74eec98647758de6258a824cbf
choscion <106219867+choscion@users.noreply.github.com>
2022-05-27 04:08:33 +0900

    3차

--- asset/js/common.js
+++ asset/js/common.js
@@ -1,119 +1,149 @@
 $(function(){
-    $(window).scroll(function(){
-        crrt = $(this).scrollTop();
-        trgt = $('.sc-visual').offset().top;
-        blck1 = $('.sc-work').offset().top-90;
-        wht1 = $('.sc-strength').offset().top-90;
-        blck2 = $('.sc-power').offset().top-90;
-        wht2 = $('footer').offset().top-90;
-
-        if (crrt > 0) {
-            $('.top-header').addClass('hide');
-            $('.etc-msg').addClass('hide');
-        } else {
-            $('.top-header').removeClass('hide');
-            $('.etc-msg').removeClass('hide');
-        }
-
-        if (crrt > trgt+200) {
-            $('.active-header').addClass('show');
-        } else {
-            $('.active-header').removeClass('show');
-        }
-
-        if (crrt > blck1 && crrt < wht1 || crrt > blck2 && crrt < wht2) {
-            $('.active-header').addClass('black');
-        } else{
-            $('.active-header').removeClass('black');
-        }
-    })
-    $(window).trigger('scroll');
-
-
-    //메뉴 hover시
-    $('.menu-item a').mouseover(function(){
-        img =$(this).data('img');
-        $(img).addClass('on').siblings().removeClass('on');
-    })
-
     
-    menuMo = gsap.timeline({
-        defaults:{
-            ease:'ease-in-out'
-        }
-    })
-    menuMo.addLabel('m1')
-    .to('.menu-open',{'z-index':1,opacity:0},'m1')
-    .to('.menu-open .txt',.5,{rotation:-40,y:-100+'%',opacity:0},'m1')
-    .to('.ham',{x:100,stagger:.1},'m1')
-    .to('.menu-close',{'z-index':2,opacity:1},'m1')
-    .to('.menu-close .txt',{rotation:0,y:0,opacity:1},'m1')
-    .to('.menu-item .underline, .sns-item .underline',.8,{rotation:0,y:0,opacity:1})
-    // .to('.sns-item .underline',.5,{rotation:0,y:0,opacity:1})
-    // .to('.cls',{opacity:1})
-    // .to('.page-wrapper',{transform:'translate(0px, 468.5px) rotate(7deg) scale(1.3, 1.3)'})
-    menuMo.pause();
 
-    $('.menu-open').click(function(e){
-        e.preventDefault();
-        menuMo.play();
-        $(this).siblings('.menu-wrap').addClass('on');
-        $('body').addClass('noscroll');
+    ////////////// matchMedia
+    gsap.registerPlugin(ScrollTrigger);
 
-        // $(this).toggleClass('on');
-        // $(this).siblings('.menu-wrap').toggleClass('on');
-    })
-    $('.menu-close').click(function(e){
-        e.preventDefault();
-        menuMo.reverse();
-        $(this).siblings('.menu-wrap').removeClass('on');
-        $('body').removeClass('noscroll');
-
-        // $(this).toggleClass('on');
-        // $(this).siblings('.menu-wrap').toggleClass('on');
-    })
-   
-    ////////////
-    $('.btn-work').click(function(e){
-        e.preventDefault();
-        var pos = $('.sc-work').offset();
-        $('body').stop().animate({scrollTop:pos.top},500);
-    })
+    ScrollTrigger.matchMedia({
+      "(max-width: 767px)": function (){ // 767px 이하
+        $(window).scroll(function(){
+            crrt = $(this).scrollTop();
+            trgt = $('.sc-visual').offset().top;
+            blck1 = $('.sc-work').offset().top-90;
+            wht1 = $('.sc-strength').offset().top-90;
+            blck2 = $('.sc-power').offset().top-90;
+            wht2 = $('footer').offset().top-90;
     
-    //////
-    MotionFooter = gsap.timeline({
-        scrollTrigger:{
-          trigger:'footer',
-          start:'300% bottom', //[트리거기준,윈도우기준]
-          end:'390% bottom',
-        //   markers:true,
-            scrub:1,
-          },
-      })
-      MotionFooter.to('.ft-inner',.7,{transform:'translate(0, 0)'})
-
-    $('.btn-top').click(function(e){
-        e.preventDefault();
-
-        gsap.to('html,body',{
-            scrollTop: 'body',
-            duration: .3,
+    
+            if (crrt > trgt) {
+                $('.active-header').addClass('show');
+            } else {
+                $('.active-header').removeClass('show');
+            }
+    
+            if (crrt > blck1 && crrt < wht1 || crrt > blck2 && crrt < wht2) {
+                $('.active-header').addClass('black');
+            } else{
+                $('.active-header').removeClass('black');
+            }
         })
-    })
-
-    $('.link-goto').click(function(e){
-        e.preventDefault();
-        trgt = $(this).data('target');
-        trgtOffset = $(trgt).offset().top;
-
-        menuMo.reverse();
-        $('.menu-wrap').removeClass('on');
-        $('body').removeClass('noscroll');
+        $(window).trigger('scroll');
         
-        gsap.to('html,body',{
-            scrollTop: trgtOffset,
-            duration: .3,
-        })
-    })
+      }, // 767px 이하 끝
 
+      "all": function () { // 모든 구간
+
+        $(window).scroll(function(){
+            crrt = $(this).scrollTop();
+            trgt = $('.sc-visual').offset().top;
+            blck1 = $('.sc-work').offset().top-90;
+            wht1 = $('.sc-strength').offset().top-90;
+            blck2 = $('.sc-power').offset().top-90;
+            wht2 = $('footer').offset().top-90;
+    
+            if (crrt > 0) {
+                $('.top-header').addClass('hide');
+                $('.etc-msg').addClass('hide');
+            } else {
+                $('.top-header').removeClass('hide');
+                $('.etc-msg').removeClass('hide');
+            }
+    
+            if (crrt > trgt+200) {
+                $('.active-header').addClass('show');
+            } else {
+                $('.active-header').removeClass('show');
+            }
+    
+            if (crrt > blck1 && crrt < wht1 || crrt > blck2 && crrt < wht2) {
+                $('.active-header').addClass('black');
+            } else{
+                $('.active-header').removeClass('black');
+            }
+        })
+        $(window).trigger('scroll');
+    
+    
+        //메뉴 hover시
+        $('.menu-item a').mouseover(function(){
+            img =$(this).data('img');
+            $(img).addClass('on').siblings().removeClass('on');
+        })
+    
+        
+        menuMo = gsap.timeline({
+            defaults:{
+                ease:'ease-in-out'
+            }
+        })
+        menuMo.addLabel('m1')
+        .to('.menu-open',{'z-index':1,opacity:0},'m1')
+        .to('.menu-open .txt',.5,{rotation:-40,y:-100+'%',opacity:0},'m1')
+        .to('.ham',{x:100,stagger:.1},'m1')
+        .to('.menu-close',{'z-index':2,opacity:1,visibility:'visible'},'m1')
+        .to('.menu-close .txt',{rotation:0,y:0,opacity:1},'m1')
+        .to('.menu-item .underline, .sns-item .underline',.8,{rotation:0,y:0,opacity:1})
+        menuMo.pause();
+    
+        $('.menu-open').click(function(e){
+            e.preventDefault();
+            menuMo.play();
+            $(this).siblings('.menu-wrap').addClass('on');
+            $('body').addClass('noscroll');
+        })
+
+        $('.menu-close').click(function(e){
+            e.preventDefault();
+            menuMo.reverse();
+            $(this).siblings('.menu-wrap').removeClass('on');
+            $('body').removeClass('noscroll');
+        })
+       
+        ////////////
+        $('.btn-work').click(function(e){
+            e.preventDefault();
+            var pos = $('.sc-work').offset();
+            $('body').stop().animate({scrollTop:pos.top},500);
+        })
+        
+        //////
+        MotionFooter = gsap.timeline({
+            scrollTrigger:{
+              trigger:'footer',
+              start:'200% bottom', //[트리거기준,윈도우기준]
+              end:'290% bottom',
+            //   markers:true,
+                scrub:1,
+              },
+          })
+          MotionFooter.to('.ft-inner',.7,{transform:'translate(0, 0)'})
+    
+        $('.btn-top').click(function(e){
+            e.preventDefault();
+    
+            gsap.to('html,body',{
+                scrollTop: 'body',
+                duration: .3,
+            })
+        })
+    
+        $('.link-goto').click(function(e){
+            e.preventDefault();
+            trgt = $(this).data('target');
+            trgtOffset = $(trgt).offset().top;
+    
+            menuMo.reverse();
+            $('.menu-wrap').removeClass('on');
+            $('body').removeClass('noscroll');
+            
+            gsap.to('html,body',{
+                scrollTop: trgtOffset,
+                duration: .3,
+            })
+        })
+
+        ScrollTrigger.refresh();
+      }
+    })
+    
 }) //Do not touch!!!
--- asset/js/main.js
+++ asset/js/main.js
@@ -1,164 +1,230 @@
 $(function(){
-    $(window).mousemove(function(e){
-      e.preventDefault();
-      gsap.to('.mouse-scroll',{
-        // visibility: 'visible',
-        // opacity: 1,
-        x:e.clientX,
-        y:e.clientY,
-        duration:.2,
-        // ease:"none",
-        })
-    })
-
-    $('a, .intro-msg, .main-msg,.sc-work .imgbox04, .sc-new .title, .power-msg, .story-msg').mouseover(function(){
-      gsap.to('.mouse-scroll',{
-        opacity: 0,
-        scale:1.2,
-      })
-    })
-    $('a, .intro-msg, .main-msg,.sc-work .imgbox04, .sc-new .title, .power-msg, .story-msg').mouseleave(function(){
-      gsap.to('.mouse-scroll',{
-        opacity: 1,
-        scale:1,
-      })
-    })
-
-    ///////헤더, visual 첫 스크롤
-    firstMoHd = gsap.timeline({
-      scrollTrigger:{
-          trigger:'main',
-          start:'top 5%', //[트리거기준,윈도우기준]
-          // markers:true,
-      }
-    })
-    firstMoHd.addLabel('MoHd');
-    firstMoHd.to('.logo',.8,{y:0,rotation:0,opacity:1},'MoHd')
-    .to('.top-nav .nav-item',.8,{y:0,rotation:0,opacity:1,stagger:.05},'MoHd')
-    .to('.sc-visual .intro-msg .mask-txt, .sc-visual .title .mask-txt',.8,{y:0,rotation:0,opacity:1,stagger:.2},'MoHd')
-
-    ////menu click
-    $('.nav-item a, .menu-item a, .sub-menu-item a, .link-move').click(function(e){
-      e.preventDefault();
-
-      trgt = $(this).data('target');
-      trgtOffset = $(trgt).offset().top;
-
-      menuMo.reverse();
-      $('.menu-wrap').removeClass('on');
-      $('body').removeClass('noscroll');
-        
-      gsap.to('html,body',{
-        scrollTop:trgtOffset,
-        duration: .5,
-      })
-
-    })
-
-
-    ////////////////////////   sc-visual   ////////////////////////
-    $('.particle, .img-bg').each(function (index,bg) {
-      scrubVal = $(this).data('scrub');
-      rotVal = $(this).data('rot');
-      xVal = $(this).data('x');
-      yVal = $(this).data('y');
-      scaleVal = $(this).data('scale');
-      blurVal = $(this).data('blur');
-      opacityVal = $(this).data('opacity');
-
-      scrollMoVisual = gsap.timeline({
-        scrollTrigger:{
-          trigger:'.sc-visual',
-          start:'top top',
-          end:'bottom top',
-          // markers:true,
-          scrub:scrubVal,
-        }
-      })
-      scrollMoVisual.addLabel('MoVisual')
-      scrollMoVisual.to('.bg-area .img-bg',{opacity:0.6},'MoVisual')
-      scrollMoVisual.to('.bg-area .img-bg .bg',{y:-100},'MoVisual')
-      scrollMoVisual.to('.bg-area .particle-01',{yPercent:yVal, rotation: rotVal},'MoVisual')
-      scrollMoVisual.to('.bg-area .particle-02',{xPercent:xVal},'MoVisual')
-      scrollMoVisual.to('.bg-area .particle-03',{scaleX:scaleVal, scaleY:scaleVal, 'filter':blurVal},'MoVisual')
-      // scrollMoVisual.to('.bg-area .particle-04',{opacity:opacityVal})
-      })
-
-
-    ////////////////////////   sc-work   ////////////////////////
-
-    ///////work 첫 스크롤
-    firstMoWork = gsap.timeline({
-      scrollTrigger:{
-          trigger:'.sc-work',
-          start:'top center', //[트리거기준,윈도우기준]
-          // markers:true,
-      }
-    })
-    firstMoWork.addLabel('MoWork');
-    firstMoWork.to('.sc-work .mask-txt',.8,{y:0,rotation:0,opacity:1},'MoWork')
-
-    ///// work parallex scroll 효과
-    $('.imgbox-item').each(function(index,item){
-      yVal = $(this).data('y');
-      scrubVal = $(this).data('scrub');
-
-      gsap.to($(this),{
-        scrollTrigger:{
-          trigger:$(this),
-          start:'top bottom', //[트리거기준,윈도우기준]
-          end:'bottom top',
-          // markers:true,
-          scrub:scrubVal,
-          },
-          yPercent:yVal
-      })
-    })
-
-    ////////////////////////   sc-strength   ////////////////////////
-    //sc-strength bg scroll
-    scrollMoStrength = gsap.timeline({
-      scrollTrigger:{
-        trigger:'.sc-strength',
-        start:'top top', //[트리거기준,윈도우기준]
-        end:'+=300%',
-      //   markers:true,
-        scrub:1,
-        pin:true,
-        },
-    })
-    scrollMoStrength.addLabel('MoStrength');
-    scrollMoStrength.from('.sc-strength video',{transform:'scale(0.3)'},'MoStrength')
-    .to('.sc-strength .title span',{x:0},'MoStrength')
-
-    ////////////////////////   sc-power   ////////////////////////
-
-    ///////power 첫 스크롤
-    firstMoPower = gsap.timeline({
-      scrollTrigger:{
-          trigger:'.sc-power .content-area',
-          start:'top center', //[트리거기준,윈도우기준]
-          // markers:true,
-      }
-    })
-    firstMoPower.addLabel('MoPower');
-    firstMoPower.to('.sc-power .mask-txt',.8,{y:0,rotation:0,opacity:1},'MoPower')
     
-    //sc-power img scroll
-    scrollMoPower = gsap.timeline({
-      scrollTrigger:{
-        trigger:'.sc-power',
-        start:'-30% top', //[트리거기준,윈도우기준]
-        end:'80% top',
-      //   markers:true,
-        scrub:1,
-        },
+
+
+    
+
+    
+
+    ////////////// matchMedia
+    gsap.registerPlugin(ScrollTrigger);
+
+    ScrollTrigger.matchMedia({
+      "(min-width: 768px)": function (){ // 768px 이상
+        ///// work parallex scroll 효과
+        $('.imgbox-item').each(function(index,item){
+          yVal = $(this).data('y');
+          scrubVal = $(this).data('scrub');
+        
+          gsap.to($(this),{
+            scrollTrigger:{
+              trigger:$(this),
+              start:'top bottom', //[트리거기준,윈도우기준]
+              end:'bottom top',
+              // markers:true,
+              scrub:scrubVal,
+              },
+              yPercent:yVal
+          })
+        })
+      },
+
+      "(max-width: 767px)": function (){ // 767px 이하
+        $('.particle, .img-bg').each(function (index,bg) {
+          scrubVal = $(this).data('scrub');
+          rotVal = $(this).data('rot');
+          xVal = $(this).data('x');
+          yVal = $(this).data('y');
+          scaleVal = $(this).data('scale');
+          blurVal = $(this).data('blur');
+          opacityVal = $(this).data('opacity');
+    
+          scrollMoVisual = gsap.timeline({
+            scrollTrigger:{
+              trigger:'.sc-visual',
+              start:'top top',
+              end:'+=200%',
+              // markers:true,
+              pin:true,
+              scrub:scrubVal,
+            }
+          })
+          scrollMoVisual.addLabel('MoVisual')
+          scrollMoVisual.to('.bg-area .img-bg',{opacity:0.6},'MoVisual')
+          scrollMoVisual.to('.bg-area .img-bg .bg',{y:-10},'MoVisual')
+          scrollMoVisual.to('.bg-area .particle-01',{yPercent:yVal, rotation: rotVal},'MoVisual')
+          scrollMoVisual.to('.bg-area .particle-02',{xPercent:xVal},'MoVisual')
+          scrollMoVisual.to('.bg-area .particle-03',{scaleX:scaleVal, scaleY:scaleVal, 'filter':blurVal},'MoVisual')
+          .to('.sc-visual .content-area',{yPercent:-30},'MoVisual')
+          .to('.etc-area',{yPercent:-500},'MoVisual')
+          })
+
+          //sc-power img scroll
+        scrollMoPower = gsap.timeline({
+          scrollTrigger:{
+            trigger:'.sc-power',
+            start:'110% top', //[트리거기준,윈도우기준]
+            end:'210% top',
+            // markers:true,
+            scrub:1,
+            },
+        })
+        scrollMoPower.addLabel('MoPower');
+        scrollMoPower.to('.sc-power .imgbox02',{top: '6vw',left: '5vw'},'MoPower')
+        .to('.sc-power .imgbox03',{top: '3.5vw',right: '12vw'},'MoPower')
+        .to('.sc-power .imgbox04',{top: '52vw',left: '10vw'},'MoPower')
+        .to('.sc-power .imgbox05',{top:'49.5vw',right: '-7vw'},'MoPower')
+
+
+          
+      }, // 767px 이하 끝
+
+      "all": function () { // 모든 구간
+
+        $(window).mousemove(function(e){
+          e.preventDefault();
+          gsap.to('.mouse-scroll',{
+            x:e.clientX,
+            y:e.clientY,
+            duration:.2,
+            })
+        })
+    
+        $('a, .intro-msg, .main-msg,.sc-work .imgbox04, .sc-new .title, .power-msg, .story-msg').mouseover(function(){
+          gsap.to('.mouse-scroll',{
+            opacity: 0,
+            scale:1.2,
+          })
+        })
+        $('a, .intro-msg, .main-msg,.sc-work .imgbox04, .sc-new .title, .power-msg, .story-msg').mouseleave(function(){
+          gsap.to('.mouse-scroll',{
+            opacity: 1,
+            scale:1,
+          })
+        })
+    
+        ///////헤더, visual 첫 스크롤
+        firstMoHd = gsap.timeline({
+          scrollTrigger:{
+              trigger:'main',
+              start:'top 5%', //[트리거기준,윈도우기준]
+              // markers:true,
+          }
+        })
+        firstMoHd.addLabel('MoHd');
+        firstMoHd.to('.logo',.8,{y:0,rotation:0,opacity:1},'MoHd')
+        .to('.top-nav .nav-item',.8,{y:0,rotation:0,opacity:1,stagger:.05},'MoHd')
+        .to('.sc-visual .intro-msg .mask-txt, .sc-visual .title .mask-txt',.8,{y:0,rotation:0,opacity:1,stagger:.2},'MoHd')
+    
+        ////menu click
+        $('.nav-item a, .menu-item a, .sub-menu-item a, .link-move').click(function(e){
+          e.preventDefault();
+    
+          trgt = $(this).data('target');
+          trgtOffset = $(trgt).offset().top;
+    
+          menuMo.reverse();
+          $('.menu-wrap').removeClass('on');
+          $('body').removeClass('noscroll');
+            
+          gsap.to('html,body',{
+            scrollTop:trgtOffset,
+            duration: .5,
+          })
+    
+        })
+    
+    
+        ////////////////////////   sc-visual   ////////////////////////
+        $('.particle, .img-bg').each(function (index,bg) {
+          scrubVal = $(this).data('scrub');
+          rotVal = $(this).data('rot');
+          xVal = $(this).data('x');
+          yVal = $(this).data('y');
+          scaleVal = $(this).data('scale');
+          blurVal = $(this).data('blur');
+          opacityVal = $(this).data('opacity');
+    
+          scrollMoVisual = gsap.timeline({
+            scrollTrigger:{
+              trigger:'.sc-visual',
+              start:'top top',
+              end:'bottom top',
+              // markers:true,
+              scrub:scrubVal,
+            }
+          })
+          scrollMoVisual.addLabel('MoVisual')
+          scrollMoVisual.to('.bg-area .img-bg',{opacity:0.6},'MoVisual')
+          scrollMoVisual.to('.bg-area .img-bg .bg',{y:-100},'MoVisual')
+          scrollMoVisual.to('.bg-area .particle-01',{yPercent:yVal, rotation: rotVal},'MoVisual')
+          scrollMoVisual.to('.bg-area .particle-02',{xPercent:xVal},'MoVisual')
+          scrollMoVisual.to('.bg-area .particle-03',{scaleX:scaleVal, scaleY:scaleVal, 'filter':blurVal},'MoVisual')
+          })
+    
+          
+        ////////////////////////   sc-work   ////////////////////////
+    
+        ///////work 첫 스크롤
+        firstMoWork = gsap.timeline({
+          scrollTrigger:{
+              trigger:'.sc-work',
+              start:'top center', //[트리거기준,윈도우기준]
+              // markers:true,
+          }
+        })
+        firstMoWork.addLabel('MoWork');
+        firstMoWork.to('.sc-work .mask-txt',.8,{y:0,rotation:0,opacity:1},'MoWork')
+
+        ////////////////////////   sc-strength   ////////////////////////
+        //sc-strength bg scroll
+        scrollMoStrength = gsap.timeline({
+          scrollTrigger:{
+            trigger:'.sc-strength',
+            start:'top top', //[트리거기준,윈도우기준]
+            end:'+=200%',
+          //   markers:true,
+            scrub:1,
+            pin:true,
+            },
+        })
+        scrollMoStrength.addLabel('MoStrength');
+        scrollMoStrength.from('.sc-strength video',{transform:'scale(0.3)'},'MoStrength')
+        .to('.sc-strength .title span',{x:0},'MoStrength')
+
+        ////////////////////////   sc-power   ////////////////////////
+
+        ///////power 첫 스크롤
+        firstMoPower = gsap.timeline({
+          scrollTrigger:{
+              trigger:'.sc-power .content-area',
+              start:'top center', //[트리거기준,윈도우기준]
+              // markers:true,
+          }
+        })
+        firstMoPower.addLabel('MoPower');
+        firstMoPower.to('.sc-power .mask-txt',.8,{y:0,rotation:0,opacity:1},'MoPower')
+
+
+        //sc-power img scroll
+        scrollMoPower = gsap.timeline({
+          scrollTrigger:{
+            trigger:'.sc-power',
+            start:'-30% top', //[트리거기준,윈도우기준]
+            end:'80% top',
+          //   markers:true,
+            scrub:1,
+            },
+        })
+        scrollMoPower.addLabel('MoPower');
+        scrollMoPower.to('.sc-power .imgbox02',{top: '6vw',left: '13vw'},'MoPower')
+        .to('.sc-power .imgbox03',{top: '2vw',right: '24vw'},'MoPower')
+        .to('.sc-power .imgbox04',{top: '38vw',left: '5vw'},'MoPower')
+        .to('.sc-power .imgbox05',{right: '7vw'},'MoPower')
+
+        ScrollTrigger.refresh();
+        
+      }
     })
-    scrollMoPower.addLabel('MoPower');
-    scrollMoPower.to('.sc-power .imgbox02',{top: '6vw',left: '13vw'},'MoPower')
-    .to('.sc-power .imgbox03',{top: '2vw',right: '24vw'},'MoPower')
-    .to('.sc-power .imgbox04',{top: '38vw',left: '5vw'},'MoPower')
-    .to('.sc-power .imgbox05',{right: '7vw'},'MoPower')
-
-
+    
 })
